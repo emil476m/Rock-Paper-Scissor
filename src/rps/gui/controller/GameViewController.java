ec9e1fc47ab6f8bb3ab70b86ce99cb7ffb05c3ce
emil476m <emilwiltmann.dk@gmail.com>
2023-02-28 10:12:52 +0100

    made fxml id

--- src/rps/gui/controller/GameViewController.java
+++ src/rps/gui/controller/GameViewController.java
@@ -5,6 +5,8 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -15,6 +17,10 @@ import java.util.ResourceBundle;
  */
 public class GameViewController implements Initializable {
 
+    public ImageView imgPlayer;
+    public Label lblPlayer;
+    public ImageView imgBot;
+    public Label lblBot;
     @FXML
     private Button btnScissor;
     @FXML
