45fe98ce76ece3fa01f35d7b33161bbcd9592595
oliverager <114471849+oliverager@users.noreply.github.com>
2023-02-28 12:24:44 +0100

    can run

--- src/rps/gui/controller/GameViewController.java
+++ src/rps/gui/controller/GameViewController.java
@@ -6,10 +6,14 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
+import java.io.File;
 import java.net.URL;
 import java.util.ResourceBundle;
+
+import static rps.bll.game.Move.Rock;
 
 /**
  *
@@ -21,6 +25,12 @@ public class GameViewController implements Initializable {
     public Label lblPlayer;
     public ImageView imgBot;
     public Label lblBot;
+    public Label aICounter;
+    public Label tieCounter;
+    public Label playerCounter;
+    public ImageView imgRock;
+    public ImageView imgPaper;
+    public ImageView imgScissor;
     @FXML
     private Button btnScissor;
     @FXML
@@ -37,17 +47,36 @@ public class GameViewController implements Initializable {
     }
 
     public void handleRock(ActionEvent actionEvent) {
+        CallRock();
     }
 
     public void handlePaper(ActionEvent actionEvent) {
+        CallPaper();
     }
 
     public void handleScissor(ActionEvent actionEvent) {
+        CallScissor();
     }
 
     public void HandleExit(ActionEvent event) {
     }
 
     public void HandleHistory(ActionEvent event) {
+    }
+
+    public void CallRock(){
+        File file = new File("data/the_Rock-removebg-preview.png");
+        Image image = new Image(file.toURI().toString());
+        imgPlayer.setImage(image);
+    }
+    public void CallPaper(){
+        File file = new File("data/paper-removebg-preview.png");
+        Image image = new Image(file.toURI().toString());
+        imgPlayer.setImage(image);
+    }
+    public void CallScissor(){
+        File file = new File("data/gZiXk0l-removebg-preview.png");
+        Image image = new Image(file.toURI().toString());
+        imgPlayer.setImage(image);
     }
 }
