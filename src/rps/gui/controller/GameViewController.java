468845d565bfd5fcd624798b89e157c28ad81098
emil476m <emilwiltmann.dk@gmail.com>
2023-02-28 10:02:42 +0100

    made changes to fxml

--- src/rps/gui/controller/GameViewController.java
+++ src/rps/gui/controller/GameViewController.java
@@ -1,7 +1,11 @@
 package rps.gui.controller;
 
 // Java imports
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -11,11 +15,27 @@ import java.util.ResourceBundle;
  */
 public class GameViewController implements Initializable {
 
+    @FXML
+    private Button btnScissor;
+    @FXML
+    private Button btnPaper;
+    @FXML
+    private Button btnRock;
+
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
+    }
+
+    public void handleRock(ActionEvent actionEvent) {
+    }
+
+    public void handlePaper(ActionEvent actionEvent) {
+    }
+
+    public void handleScissor(ActionEvent actionEvent) {
     }
 }
